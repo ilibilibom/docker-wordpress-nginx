53e0d89671b1c05414af607156e45891856633e5
Ilan Diamond <iland@jfrog.com>
2017-08-22 13:34:57 +0300

    Change user id

--- docker/php-fpm/docker-php-entrypoint.sh
+++ docker/php-fpm/docker-php-entrypoint.sh
@@ -1,6 +1,6 @@
 # Set default WWW_DATA_USERID if not exist
 # password is limited by 8 characters
-: ${WWW_DATA_USERID:=33}
+: ${WWW_DATA_USERID:=1000}
 
 usermod -u $WWW_DATA_USERID www-data
 groupmod -g $WWW_DATA_USERID www-data
